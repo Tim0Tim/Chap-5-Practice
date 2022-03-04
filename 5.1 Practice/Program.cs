65416984ae91a6bdbf205b22921e2239ec3619de
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-03-04 10:51:47 -0700

    Add project files.

---
+++ 5.1 Practice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _5._1_Practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            int total = 0;
+            int gradeCounter = 0;
+
+            Console.Write("Enter grade or -1 to quit: ");
+            int grade = int.Parse(Console.ReadLine());
+
+            while (grade != -1)
+            {
+                total = total + grade;
+                gradeCounter = gradeCounter + 1;
+
+                Console.Write("Enter grade or -1 to quit: ");
+                grade = int.Parse(Console.ReadLine());
+            }
+
+            if (gradeCounter !=0)
+            {
+                double average = (double)total / gradeCounter;
+                Console.WriteLine($"\nTotal of the {gradeCounter} grades entered is {total}");
+                Console.WriteLine($"Class average is {average:F}");
+            }
+            else
+            {
+                Console.WriteLine("No grades were entered");
+            }
+        }
+    }
+}
